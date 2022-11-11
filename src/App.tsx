866df0431bdc33fe9ed6373ffe30d31c60c446da
jun <kimsj012731@gmail.com>
2022-11-11 18:39:15 +0900

    Update gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [time, setTime] = useState(new Date());
@@ -8,11 +8,11 @@ function App() {
   }, 1000);
 
   useEffect(() => {
-    clearInterval(timer)
+    clearInterval(timer);
   }, [timer]);
 
   let isEven = true;
-  if (time.getTime() % 2 === 0){
+  if (time.getTime() % 2 === 0) {
     isEven = true;
   } else {
     isEven = false;
@@ -20,20 +20,19 @@ function App() {
 
   return (
     //DOM
-    <div >
-    <h3>현재 시간: { time.toLocaleTimeString() }</h3>
-    <h3>지금 시간은 홀일까요 짝일까요? = {isEven ? '짝이네' : '홀이네'}</h3>
-    <h3>자기소개</h3>
-    <h3>이름: 김성준</h3>
-    <h3>학력</h3>
-    <>
-    <li>덕일전자공업고등학교</li>
-    <li>동양미래대학교</li>
-    </>
-    <h3>경력</h3>
-    <>
-    <li>SKT</li>
-    </>
+    <div>
+      <h3>현재 시간: {time.toLocaleTimeString()}</h3>
+      <h3>자기소개</h3>
+      <h3>이름: 김성준</h3>
+      <h3>학력</h3>
+      <>
+        <li>덕일전자공업고등학교</li>
+        <li>동양미래대학교</li>
+      </>
+      <h3>경력</h3>
+      <>
+        <li>SKT</li>
+      </>
     </div>
   );
 }
